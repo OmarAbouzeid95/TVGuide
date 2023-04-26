cfa6e202398a1a4af63049f1c9ffbfa68c9f2500
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2023-04-26 13:43:02 -0400

    remodified the CORS

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { getDb, dbConnect } = require('./db')
 const app = express()
 
 
-app.use(cors({ origin: process.env.FRONTEND_URL,
+app.use(cors({ origin: 'https://omarabouzeid95.github.io',
     allowedHeaders: [
       'access-control-allow-origin',
       'authorization',
